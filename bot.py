ba3467bdb837f5a0a4114e228c666f56dcb5ceda
muxailk <muxail1327@mail.ru>
2022-12-03 21:17:50 +0300

    Added error handling and logging

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import requests
 import json
 import datetime
 import os
+from config import *
 from aiogram import Bot, Dispatcher, executor, types
 
 
@@ -13,41 +14,44 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=['subscribe'])
-async def subscribe(message: types.Message):
-    await message.answer("Вы успешно подписались на рассылку!\nЖдите, скоро выйдут новые обзоры и вы узнаете о них первыми =)")
+# @dp.message_handler(commands=['subscribe'])
+# async def subscribe(message: types.Message):
+#     await message.answer("Вы успешно подписались на рассылку!\nЖдите, скоро выйдут новые обзоры и вы узнаете о них первыми =)")
 
 
-@dp.message_handler(commands=['unsubscribe'])
-async def unsubscribe(message: types.Message):
-    await message.answer("Вы успешно отписаны от рассылки.")
+# @dp.message_handler(commands=['unsubscribe'])
+# async def unsubscribe(message: types.Message):
+#     await message.answer("Вы успешно отписаны от рассылки.")
 
 
 def get_posts():
-    url = f"https://api.vk.com/method/wall.get?v=5.131&access_token={APP_TOKEN}&scope=photos,audio&owner_id={TARGET_ID}"
-    response = requests.get(url).json()
+    try:
+        url = f"https://api.vk.com/method/wall.get?v=5.131&access_token={APP_TOKEN}&scope=photos,audio&owner_id={TARGET_ID}"
+        response = requests.get(url).json()
 
-    result = []
-    items = response['response']['items']
+        result = []
+        items = response['response']['items']
 
-    for i in range(0, len(items)):
-        item = items[i]
+        for i in range(0, len(items)):
+            item = items[i]
 
-        if isStored(item['hash']):
-            continue
+            if isStored(item['hash']):
+                continue
 
-        for attachment in item['attachments']:
-            if attachment['type'] == 'photo':
-                photo = get_highest_quality_photo(attachment['photo']['sizes'])
+            for attachment in item['attachments']:
+                if attachment['type'] == 'photo':
+                    photo = get_highest_quality_photo(attachment['photo']['sizes'])
 
-        store(item['hash'])
+            store(item['hash'])
 
-        if len(result) < i + 1:
-            result.append({})
+            if len(result) < i + 1:
+                result.append({})
 
-        result[i]['photo'] = photo
+            result[i]['photo'] = photo
 
-    return result
+        return result
+    except:
+        log('Error while getting posts', 'get_posts')
 
 
 def get_highest_quality_photo(sizes):
@@ -122,61 +126,65 @@ def isTarget(id):
 
 
 def get_stories():
-    url = f"https://api.vk.com/method/stories.get?v=5.131&access_token={APP_TOKEN}&scope=stories,offline"
-    response = requests.get(url).json()
+    try:
+        url = f"https://api.vk.com/method/stories.get?v=5.131&access_token={APP_TOKEN}&scope=stories,offline"
+        response = requests.get(url).json()
 
-    result = []
-    items = response['response']['items']
+        result = []
+        items = response['response']['items']
 
-    for i in range(0, response['response']['count']):
-        item = items[i]
+        for i in range(0, response['response']['count']):
+            item = items[i]
 
-        if not isTarget(item['stories'][0]['owner_id']):
-            continue
-        print('isStored')
+            if not isTarget(item['stories'][0]['owner_id']):
+                continue
 
-        if isStored(item['id']):
-            continue
+            if isStored(item['id']):
+                continue
 
-        store(item['id'])
+            store(item['id'])
 
-        for j in range(0, len(item['stories'])):
-            photo = video = ''
-            story = item['stories'][j]
-            print('story=')
-            if story['type'] == 'photo':
-                photo = get_highest_quality_photo(story['photo']['sizes'])
-                print('photo')
+            for j in range(0, len(item['stories'])):
+                photo = video = ''
+                story = item['stories'][j]
 
-            if story['type'] == 'video':
-                video = get_video(story)
-                print('video')
+                if story['type'] == 'photo':
+                    photo = get_highest_quality_photo(story['photo']['sizes'])
 
-            if len(result) < j + 1:
-                result.append({})
+                if story['type'] == 'video':
+                    video = get_video(story)
 
-            print('Conditions passed')
-            result[j]['photo'] = photo
-            result[j]['video'] = video
-            print('info added')
+                if len(result) < j + 1:
+                    result.append({})
 
-    return result
+                result[j]['photo'] = photo
+                result[j]['video'] = video
+
+        return result
+    except:
+        log('Error while getting stories', 'get_stories')
+        return []
 
 
 async def scheduled(wait_for):
     while True:
         await asyncio.sleep(wait_for)
-        posts = get_posts()
-        stories = get_stories()
-        print(stories)
-        # for post in posts:
-        #     await bot.send_photo(CHAT_ID, post['photo'])
 
-        for story in stories:
-            if 'video' in story:
-                await bot.send_video(CHAT_ID, story['video'])
-            elif 'photo' in story:
-                await bot.send_video(CHAT_ID, story['video'])
+        try:
+            posts = get_posts()
+            stories = get_stories()
+
+            for post in posts:
+                await bot.send_photo(CHAT_ID, post['photo'])
+
+            for story in stories:
+                if 'video' in story:
+                    await bot.send_video(CHAT_ID, story['video'])
+                elif 'photo' in story:
+                    await bot.send_video(CHAT_ID, story['video'])
+        except:
+            log('Something went wrong! Error...', 'scheduled')
+            print('Error...')
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
